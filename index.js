122f196016fc8a3624600991b9f482aaadf2b149
Emna Guellaty <eguellaty@gmail.com>
2021-05-04 10:52:06 +0200

    finish

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.length = 0;
+    this.items = []
+  }
 
-  add(item) {}
+  add(item) {
+    this.length = this.items.push(item)
+    this.items.sort(function (a, b) { return a - b })
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.length || pos < 0) {
+      throw new Error("OutOfBounds")
+    } else {
+      return this.items[pos]
+    }
 
-  max() {}
+  }
 
-  min() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
 
-  sum() {}
+      return this.items[this.length - 1]
+    }
+  }
 
-  avg() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+
+      return this.items[0]
+    }
+  }
+
+  sum() {
+
+    return this.items.reduce((acc, value) => acc + value, 0)
+
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return this.sum() / this.length
+    }
+  }
 }
 
 module.exports = SortedList;
